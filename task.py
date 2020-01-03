95deb7243d78e558b4a595478765f8131a13a438
geek3000 <jonathantchuente@gmail.com>
2020-01-03 21:54:37 +0100

    Deploy on git

--- task.py
+++ task.py
@@ -6,15 +6,15 @@ def convert_to_png(file):
     img=img.convert('RGB')
     return img
 
-def scale_img(img, path, file_name):
+def scale_img(img_path, output_path, file_name):
     baseheight = 400
-    
+    img = Image.open(img_path)
     hpercent = (baseheight / float(img.size[1]))
     wsize = int((float(img.size[0]) * float(hpercent)))
     img = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
 
-    output_file=output_path+"/resized_"+file_name+'.jpg'
-    img.save(output_file, optimize=True, format='JPEG', quality=90)
+    output_file=output_path+"/resized_"+file_name
+    img.save(output_file, optimize=True, quality=90)
 
     size=0
     with open(output_file, 'rb') as q:
@@ -22,14 +22,15 @@ def scale_img(img, path, file_name):
         size=len(t)
         
     if(size<64000):
-        img.save(output_file, optimize=True, format='JPEG', quality=60)
+        img.save(output_file, optimize=True, quality=60)
 
 print("GEEK123 Use me to scale image")
 
 path=input("Enter the path of the directory: ")
 if os.path.exists(path):
+    output_path=path+'/output'
     try:
-        output_path=path+'/output'
+        
         os.mkdir(output_path)
     except:
         pass
@@ -38,7 +39,6 @@ if os.path.exists(path):
         if file.endswith(".png"):
             img_path=path+'/'+file
             print("Scaling "+img_path)
-            img=convert_to_png(img_path)
-            scale_img(img, path, file.split('.')[0])
+            scale_img(img_path, output_path, file)
     print("End scaling images")
 
